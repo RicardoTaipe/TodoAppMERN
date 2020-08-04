83fa6f27564dc7009595eaa916842c7e90fe2762
RicardoTaipe <rdavid28@hotmail.es>
2020-08-03 21:23:29 -0500

    imageURL added

--- backend-api/src/models/user.js
+++ backend-api/src/models/user.js
@@ -8,7 +8,7 @@ const UserSchema = new Schema(
     username: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    imageURL: { type: String },
+    imageURL: { type: String, default:"" },
   },
   { timestamps: true }
 );
